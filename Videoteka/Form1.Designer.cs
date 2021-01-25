d3b1fcd35027b4975c348ab11c6ddb1d42790e28
DavidD2310 <77963392+DavidD2310@users.noreply.github.com>
2021-01-25 10:54:13 +0100

    Add files via upload

---
+++ Videoteka/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Videoteka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.PotvrdiPrijavu = new System.Windows.Forms.Button();
+            this.lozinka = new System.Windows.Forms.TextBox();
+            this.korisnik = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.PotvrdiPrijavu);
+            this.groupBox1.Controls.Add(this.lozinka);
+            this.groupBox1.Controls.Add(this.korisnik);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(212, 77);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(264, 193);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Prijava";
+            // 
+            // PotvrdiPrijavu
+            // 
+            this.PotvrdiPrijavu.Location = new System.Drawing.Point(102, 146);
+            this.PotvrdiPrijavu.Margin = new System.Windows.Forms.Padding(2);
+            this.PotvrdiPrijavu.Name = "PotvrdiPrijavu";
+            this.PotvrdiPrijavu.Size = new System.Drawing.Size(70, 32);
+            this.PotvrdiPrijavu.TabIndex = 4;
+            this.PotvrdiPrijavu.Text = "Potvrdi";
+            this.PotvrdiPrijavu.UseVisualStyleBackColor = true;
+            this.PotvrdiPrijavu.Click += new System.EventHandler(this.PotvrdiPrijavu_Click);
+            // 
+            // lozinka
+            // 
+            this.lozinka.Location = new System.Drawing.Point(142, 95);
+            this.lozinka.Margin = new System.Windows.Forms.Padding(2);
+            this.lozinka.Name = "lozinka";
+            this.lozinka.Size = new System.Drawing.Size(76, 20);
+            this.lozinka.TabIndex = 3;
+            this.lozinka.Text = "admin";
+            // 
+            // korisnik
+            // 
+            this.korisnik.Location = new System.Drawing.Point(142, 54);
+            this.korisnik.Margin = new System.Windows.Forms.Padding(2);
+            this.korisnik.Name = "korisnik";
+            this.korisnik.Size = new System.Drawing.Size(76, 20);
+            this.korisnik.TabIndex = 2;
+            this.korisnik.Text = "admin";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 95);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Lozinka";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 54);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Korisničko ime";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(698, 366);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button PotvrdiPrijavu;
+        private System.Windows.Forms.TextBox lozinka;
+        private System.Windows.Forms.TextBox korisnik;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Videoteka/Form2.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Videoteka
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Close = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.iznajmi = new System.Windows.Forms.Button();
+            this.pregledaj = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Close
+            // 
+            this.Close.Location = new System.Drawing.Point(41, 291);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(108, 34);
+            this.Close.TabIndex = 0;
+            this.Close.Text = "button1";
+            this.Close.UseVisualStyleBackColor = true;
+            this.Close.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.Close);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.iznajmi);
+            this.panel1.Controls.Add(this.pregledaj);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 1;
+            // 
+            // iznajmi
+            // 
+            this.iznajmi.Location = new System.Drawing.Point(41, 28);
+            this.iznajmi.Name = "iznajmi";
+            this.iznajmi.Size = new System.Drawing.Size(108, 41);
+            this.iznajmi.TabIndex = 2;
+            this.iznajmi.Text = "iznajmi";
+            this.iznajmi.UseVisualStyleBackColor = true;
+            // 
+            // pregledaj
+            // 
+            this.pregledaj.Location = new System.Drawing.Point(41, 85);
+            this.pregledaj.Name = "pregledaj";
+            this.pregledaj.Size = new System.Drawing.Size(108, 38);
+            this.pregledaj.TabIndex = 3;
+            this.pregledaj.Text = "pregledaj";
+            this.pregledaj.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(41, 143);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(108, 34);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "vrati";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(41, 193);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(108, 39);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "pregledaj filmove";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.panel3.Location = new System.Drawing.Point(193, 12);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(595, 331);
+            this.panel3.TabIndex = 2;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Close;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button iznajmi;
+        private System.Windows.Forms.Button pregledaj;
+    }
+}
