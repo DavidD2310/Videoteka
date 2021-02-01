43e9445c24092e166bfab42fc63d25d377be29b2
DavidD2310 <77963392+DavidD2310@users.noreply.github.com>
2021-02-01 11:59:03 +0100

    Add files via upload

---
+++ Form2.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Videoteka
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+
+        }
+
+
+        private void Close_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 form1 = new Form1();
+            form1.Closed += (s, args) => this.Close();
+            form1.Show();
+        }
+
+        private void Panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Iznajmi_Click(object sender, EventArgs e)
+        {
+            ImeProzor.Text = "Pregedaj filmove";
+            this.Prozori.Controls.Clear();
+            Pregledaj pregledaj = new Pregledaj()
+            {
+                Dock = DockStyle.Fill,
+                TopLevel = false,
+                TopMost = true
+            };
+            this.Prozori.Controls.Add(pregledaj);
+            pregledaj.Show();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            ImeProzor.Text = "Iznajmi";
+            this.Prozori.Controls.Clear();
+            iznajmi inzajmi = new iznajmi()
+            {
+                Dock = DockStyle.Fill,
+                TopLevel = false,
+                TopMost = true
+            };
+            this.Prozori.Controls.Add(Pregledaj);
+            Pregledaj.Show();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            ImeProzor.Text = "vrati";
+            this.Prozori.Controls.Clear();
+            vrati vrati  = new vrati()
+            {
+                Dock = DockStyle.Fill,
+                TopLevel = false,
+                TopMost = true
+            };
+            this.Prozori.Controls.Add(vrati);
+            vrati.Show();
+        }
+    }
+}
---
+++ Pregledaj.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Videoteka
+{
+    partial class Pregledaj
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Filmovi = new System.Windows.Forms.ListBox();
+            this.količina = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // Filmovi
+            // 
+            this.Filmovi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Filmovi.FormattingEnabled = true;
+            this.Filmovi.ItemHeight = 16;
+            this.Filmovi.Location = new System.Drawing.Point(201, 111);
+            this.Filmovi.Name = "Filmovi";
+            this.Filmovi.Size = new System.Drawing.Size(120, 132);
+            this.Filmovi.TabIndex = 0;
+            // 
+            // količina
+            // 
+            this.količina.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.količina.FormattingEnabled = true;
+            this.količina.ItemHeight = 16;
+            this.količina.Location = new System.Drawing.Point(327, 111);
+            this.količina.Name = "količina";
+            this.količina.Size = new System.Drawing.Size(44, 132);
+            this.količina.TabIndex = 1;
+            // 
+            // Pregledaj
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(594, 353);
+            this.Controls.Add(this.količina);
+            this.Controls.Add(this.Filmovi);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Pregledaj";
+            this.Text = "Pregledaj";
+            this.Load += new System.EventHandler(this.Pregledaj_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox Filmovi;
+        private System.Windows.Forms.ListBox količina;
+    }
+}
