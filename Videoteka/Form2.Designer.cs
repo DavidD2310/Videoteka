95a2757e31cfaa62dc77d5fc02394a2a80cb160b
DavidD2310 <77963392+DavidD2310@users.noreply.github.com>
2021-02-01 11:47:23 +0100

    Add files via upload

--- Videoteka/Form2.Designer.cs
+++ Videoteka/Form2.Designer.cs
@@ -30,82 +30,202 @@
         {
             this.Close = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.iznajmi = new System.Windows.Forms.Button();
-            this.pregledaj = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.vrati = new System.Windows.Forms.Button();
+            this.Iznajmi = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel3 = new System.Windows.Forms.Panel();
+            this.Pregledaj = new System.Windows.Forms.Button();
+            this.iconPictureBox1 = new FontAwesome.Sharp.IconPictureBox();
+            this.iconPictureBox2 = new FontAwesome.Sharp.IconPictureBox();
+            this.iconPictureBox3 = new FontAwesome.Sharp.IconPictureBox();
+            this.iconPictureBox4 = new FontAwesome.Sharp.IconPictureBox();
+            this.Prozori = new System.Windows.Forms.Panel();
+            this.ImeProzor = new System.Windows.Forms.Label();
+            this.Vrijeme = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox4)).BeginInit();
             this.SuspendLayout();
             // 
             // Close
             // 
-            this.Close.Location = new System.Drawing.Point(41, 291);
+            this.Close.Location = new System.Drawing.Point(76, 407);
             this.Close.Name = "Close";
-            this.Close.Size = new System.Drawing.Size(108, 34);
+            this.Close.Size = new System.Drawing.Size(94, 28);
             this.Close.TabIndex = 0;
-            this.Close.Text = "button1";
+            this.Close.Text = "zatvori";
             this.Close.UseVisualStyleBackColor = true;
             this.Close.Click += new System.EventHandler(this.Close_Click);
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.iconPictureBox4);
+            this.panel1.Controls.Add(this.iconPictureBox3);
+            this.panel1.Controls.Add(this.iconPictureBox2);
+            this.panel1.Controls.Add(this.iconPictureBox1);
             this.panel1.Controls.Add(this.Close);
-            this.panel1.Controls.Add(this.button4);
-            this.panel1.Controls.Add(this.button3);
-            this.panel1.Controls.Add(this.iznajmi);
-            this.panel1.Controls.Add(this.pregledaj);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Controls.Add(this.vrati);
+            this.panel1.Controls.Add(this.Iznajmi);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.Pregledaj);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(800, 450);
             this.panel1.TabIndex = 1;
             // 
-            // iznajmi
+            // vrati
             // 
-            this.iznajmi.Location = new System.Drawing.Point(41, 28);
-            this.iznajmi.Name = "iznajmi";
-            this.iznajmi.Size = new System.Drawing.Size(108, 41);
-            this.iznajmi.TabIndex = 2;
-            this.iznajmi.Text = "iznajmi";
-            this.iznajmi.UseVisualStyleBackColor = true;
+            this.vrati.FlatAppearance.BorderSize = 0;
+            this.vrati.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.vrati.Location = new System.Drawing.Point(0, 260);
+            this.vrati.Name = "vrati";
+            this.vrati.Size = new System.Drawing.Size(200, 46);
+            this.vrati.TabIndex = 7;
+            this.vrati.Text = "vrati";
+            this.vrati.UseVisualStyleBackColor = true;
+            this.vrati.Click += new System.EventHandler(this.Button2_Click);
             // 
-            // pregledaj
+            // Iznajmi
             // 
-            this.pregledaj.Location = new System.Drawing.Point(41, 85);
-            this.pregledaj.Name = "pregledaj";
-            this.pregledaj.Size = new System.Drawing.Size(108, 38);
-            this.pregledaj.TabIndex = 3;
-            this.pregledaj.Text = "pregledaj";
-            this.pregledaj.UseVisualStyleBackColor = true;
+            this.Iznajmi.FlatAppearance.BorderSize = 0;
+            this.Iznajmi.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Iznajmi.Location = new System.Drawing.Point(3, 183);
+            this.Iznajmi.Name = "Iznajmi";
+            this.Iznajmi.Size = new System.Drawing.Size(200, 46);
+            this.Iznajmi.TabIndex = 6;
+            this.Iznajmi.Text = "iznajmi";
+            this.Iznajmi.UseVisualStyleBackColor = true;
+            this.Iznajmi.Click += new System.EventHandler(this.Button1_Click);
             // 
-            // button3
+            // panel2
             // 
-            this.button3.Location = new System.Drawing.Point(41, 143);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(108, 34);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "vrati";
-            this.button3.UseVisualStyleBackColor = true;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(206, 100);
+            this.panel2.TabIndex = 5;
             // 
-            // button4
+            // label1
             // 
-            this.button4.Location = new System.Drawing.Point(41, 193);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(108, 39);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "pregledaj filmove";
-            this.button4.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Niagara Engraved", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(85, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "videoteka";
             // 
             // panel3
             // 
             this.panel3.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.panel3.Location = new System.Drawing.Point(193, 12);
+            this.panel3.Controls.Add(this.Vrijeme);
+            this.panel3.Controls.Add(this.ImeProzor);
+            this.panel3.Controls.Add(this.Prozori);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel3.Location = new System.Drawing.Point(206, 0);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(595, 331);
+            this.panel3.Size = new System.Drawing.Size(594, 450);
             this.panel3.TabIndex = 2;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel3_Paint);
+            // 
+            // Pregledaj
+            // 
+            this.Pregledaj.FlatAppearance.BorderSize = 0;
+            this.Pregledaj.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Pregledaj.Location = new System.Drawing.Point(3, 109);
+            this.Pregledaj.Name = "Pregledaj";
+            this.Pregledaj.Size = new System.Drawing.Size(200, 46);
+            this.Pregledaj.TabIndex = 2;
+            this.Pregledaj.Text = "Pregledaj";
+            this.Pregledaj.UseVisualStyleBackColor = true;
+            this.Pregledaj.Click += new System.EventHandler(this.Iznajmi_Click);
+            // 
+            // iconPictureBox1
+            // 
+            this.iconPictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.iconPictureBox1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox1.IconChar = FontAwesome.Sharp.IconChar.Book;
+            this.iconPictureBox1.IconColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconPictureBox1.Location = new System.Drawing.Point(28, 109);
+            this.iconPictureBox1.Name = "iconPictureBox1";
+            this.iconPictureBox1.Size = new System.Drawing.Size(32, 32);
+            this.iconPictureBox1.TabIndex = 8;
+            this.iconPictureBox1.TabStop = false;
+            // 
+            // iconPictureBox2
+            // 
+            this.iconPictureBox2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.iconPictureBox2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox2.IconChar = FontAwesome.Sharp.IconChar.Camera;
+            this.iconPictureBox2.IconColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox2.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconPictureBox2.Location = new System.Drawing.Point(28, 183);
+            this.iconPictureBox2.Name = "iconPictureBox2";
+            this.iconPictureBox2.Size = new System.Drawing.Size(32, 32);
+            this.iconPictureBox2.TabIndex = 9;
+            this.iconPictureBox2.TabStop = false;
+            // 
+            // iconPictureBox3
+            // 
+            this.iconPictureBox3.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.iconPictureBox3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox3.IconChar = FontAwesome.Sharp.IconChar.Redo;
+            this.iconPictureBox3.IconColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox3.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconPictureBox3.Location = new System.Drawing.Point(28, 277);
+            this.iconPictureBox3.Name = "iconPictureBox3";
+            this.iconPictureBox3.Size = new System.Drawing.Size(32, 32);
+            this.iconPictureBox3.TabIndex = 10;
+            this.iconPictureBox3.TabStop = false;
+            // 
+            // iconPictureBox4
+            // 
+            this.iconPictureBox4.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.iconPictureBox4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox4.IconChar = FontAwesome.Sharp.IconChar.WindowClose;
+            this.iconPictureBox4.IconColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox4.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconPictureBox4.Location = new System.Drawing.Point(28, 407);
+            this.iconPictureBox4.Name = "iconPictureBox4";
+            this.iconPictureBox4.Size = new System.Drawing.Size(32, 32);
+            this.iconPictureBox4.TabIndex = 11;
+            this.iconPictureBox4.TabStop = false;
+            // 
+            // Prozori
+            // 
+            this.Prozori.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.Prozori.Location = new System.Drawing.Point(0, 97);
+            this.Prozori.Name = "Prozori";
+            this.Prozori.Size = new System.Drawing.Size(594, 353);
+            this.Prozori.TabIndex = 0;
+            // 
+            // ImeProzor
+            // 
+            this.ImeProzor.AutoSize = true;
+            this.ImeProzor.Font = new System.Drawing.Font("Niagara Engraved", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ImeProzor.Location = new System.Drawing.Point(106, 28);
+            this.ImeProzor.Name = "ImeProzor";
+            this.ImeProzor.Size = new System.Drawing.Size(0, 16);
+            this.ImeProzor.TabIndex = 1;
+            // 
+            // Vrijeme
+            // 
+            this.Vrijeme.AutoSize = true;
+            this.Vrijeme.Font = new System.Drawing.Font("Niagara Engraved", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Vrijeme.Location = new System.Drawing.Point(434, 28);
+            this.Vrijeme.Name = "Vrijeme";
+            this.Vrijeme.Size = new System.Drawing.Size(0, 16);
+            this.Vrijeme.TabIndex = 2;
             // 
             // Form2
             // 
@@ -120,6 +240,14 @@
             this.Name = "Form2";
             this.Text = "Form2";
             this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox4)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -129,9 +257,17 @@
         private System.Windows.Forms.Button Close;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button iznajmi;
-        private System.Windows.Forms.Button pregledaj;
+        private System.Windows.Forms.Button Pregledaj;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button vrati;
+        private System.Windows.Forms.Button Iznajmi;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox4;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox3;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox2;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox1;
+        private System.Windows.Forms.Label Vrijeme;
+        private System.Windows.Forms.Label ImeProzor;
+        private System.Windows.Forms.Panel Prozori;
     }
 }
--- Videoteka/Form2.cs
+++ Videoteka/Form2.cs
@@ -26,5 +26,52 @@ namespace Videoteka
             form1.Closed += (s, args) => this.Close();
             form1.Show();
         }
+
+        private void Panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Iznajmi_Click(object sender, EventArgs e)
+        {
+            ImeProzor.Text = "Pregedaj filmove";
+            this.Prozori.Controls.Clear();
+            Pregledaj pregledaj = new Pregledaj()
+            {
+                Dock = DockStyle.Fill,
+                TopLevel = false,
+                TopMost = true
+            };
+            this.Prozori.Controls.Add(pregledaj);
+            pregledaj.Show();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            ImeProzor.Text = "Iznajmi";
+            this.Prozori.Controls.Clear();
+            iznajmi inzajmi = new iznajmi()
+            {
+                Dock = DockStyle.Fill,
+                TopLevel = false,
+                TopMost = true
+            };
+            this.Prozori.Controls.Add(Pregledaj);
+            Pregledaj.Show();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            ImeProzor.Text = "vrati";
+            this.Prozori.Controls.Clear();
+            vrati vrati  = new vrati()
+            {
+                Dock = DockStyle.Fill,
+                TopLevel = false,
+                TopMost = true
+            };
+            this.Prozori.Controls.Add(vrati);
+            vrati.Show();
+        }
     }
 }
